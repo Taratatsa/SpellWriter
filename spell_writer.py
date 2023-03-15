5f630d047b1d53da918e90bd2af6d84d126a7b94
LelayJ-ESIEE <jules.lelay@edu.esiee.fr>
2023-03-15 15:15:48 +0100

    Initial commit - Starting point

---
+++ spell_writer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+This module allows the writing of spells as described in Gorilla of Destiny's spell writing guide.
+"""
+
+__author__ = 'Jules "Taratatsa" Lelay'
+__copyright__ = "None"
+__credits__ = ["Gorilla of Destiny"]
+
+__license__ = "None"
+__date__ = "2023.03.15"
+__version__ = "0.0.1"
+__maintainer__ = 'Jules "Taratatsa" Lelay'
+__email__ = "jules.lilaille@gmail.com"
+__status__ = "Development"
